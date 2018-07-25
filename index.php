9b4fd80f0c51435d88c0d5d4231e6edfc5c258db
Jack Biggin <me@jackbiggin.me>
2018-07-25 22:35:32 +0100

    Designed basic layout (and included all of those wonderful requirements)

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap/Font Awesome CSS -->
+		<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="./assets/css/fontawesomeall.min.css">
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="./assets/css/styles.css">
+		
+		<title>LiveView</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-dark custom-nav">
+			<div class="container">
+				<span class="navbar-brand mb-0 h1"><span class="title">LiveView:</span> Show name</span>
+			</div>
+		</nav>
+		
+		<div class="container">
+			<div class="header previously"><span class="title"><i class="fas fa-fw fa-arrow-left"></i> PREV:</span> Previously name</div>	
+			<div class="header now"><span class="title"><i class="fas fa-fw fa-caret-square-right"></i> NOW:</span> Now Name</div>	
+			<div class="header next"><span class="title"><i class="fas fa-fw fa-arrow-right"></i> NEXT:</span> next name</div>
+			
+			<h2 class="header section-header"><i class="fas fa-fw fa-microphone"></i> Microphones</h2>
+			<div class="section-content">...</div>
+			
+			<h2 class="header section-header"><i class="fas fa-fw fa-newspaper"></i> Script</h2>
+			<div class="section-content">...</div>
+			
+			<div class="footer"><a href="#">Enable Tech Mode</a> | <a href="#">Made with ♥ by Jack Biggin</a></div>
+		</div>
+
+		<!-- JS -->
+		<script src="./assets/js/jquery-3.3.1.slim.min.js" ></script>
+		<script src="./assets/js/popper.min.js"></script>
+		<script src="./assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
